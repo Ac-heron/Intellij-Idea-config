ed65fb308b1cc4032470fb7fbd1b6f8074a9e1d7
Acheron <acheron20122012@gmail.com>
2016-01-11 16:05:27 +0800

    我的Intellij Idea15.0.2设置;

---
+++ consoles/.history/db/5f2d7434-4159-4e90-9946-965b6e79f524.sql
@@ -0,0 +1,7 @@
+delete from EMERG_TASK where id='ca279b082df54e1e9953e48159c53686'
+;-- -. . -..- - / . -. - .-. -.--
+SELECT * from EMERG_TASK where id='ca279b082df54e1e9953e48159c53686'
+;-- -. . -..- - / . -. - .-. -.--
+delete from EMERG_TASK where id='ca279b082df54e1e9953e48159c53687'
+;-- -. . -..- - / . -. - .-. -.--
+SELECT * from EMERG_TASK
---
+++ consoles/db/5f2d7434-4159-4e90-9946-965b6e79f524/console.sql
@@ -0,0 +1,1 @@
+SELECT * from EMERG_TASK
